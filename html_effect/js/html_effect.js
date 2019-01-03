828124a1bf771b4d7b0bc4e10f500c1fbfc8541e
aob-kc <brave3719246@gmail.com>
2019-01-03 17:49:29 +0800

    up
    修改、上傳

--- html_effect/js/html_effect.js
+++ html_effect/js/html_effect.js
@@ -8,3 +8,16 @@ $(".slide_order").hover(function () {
     console.log(44544165);
     $(".slide_order_opacity").animate({width:'toggle'},500);
 });
+
+var setintervel_img_img = document.querySelector('.setintervel_img');
+var setintervel_img_cat = 'https://ichef.bbci.co.uk/news/624/cpsprodpb/14DD1/production/_103175458_p06f24vf.jpg';
+var setintervel_img_dog = 'https://cdn2.ettoday.net/images/1933/1933457.jpg';
+var imageSources = [setintervel_img_cat, setintervel_img_dog]
+		var index = 0;
+		setInterval(function () {
+			if (index === imageSources.length) {
+				index = 0;
+			}
+			setintervel_img_img.src = imageSources[index];
+			index++;
+		}, 2000);
