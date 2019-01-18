148343c16a474daf668b49c492f7da2a41e70d03
aob-kc <brave3719246@gmail.com>
2019-01-18 09:55:31 +0800

    up

--- Linli-Farmer/js/linli-js.js
+++ Linli-Farmer/js/linli-js.js
@@ -6,9 +6,32 @@ var banner_Swiper = new Swiper('#banner_swiper', {
     autoplay: {
         delay: 4000,
     },
+    effect: 'fade',
+    fade: {
+        crossFade: false,
+    },
 });
 
 // 區域移動效果
+// 重新整理後會返回top，且頁面會如同重新載入般進行頂部動畫效果
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0), 500;
+};
+
+// nav_logo
+$('#img_logo').click(function () {
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0), 500;
+    };
+});
+
+// nav
+$('#nav_about').click(function () {
+    $('html,body').animate({
+        scrollTop: ($('#about').offset().top - $('#nav').height())
+    }, 1000);
+});
+
 $('#nav_about').click(function () {
     $('html,body').animate({
         scrollTop: ($('#about').offset().top - $('#nav').height())
